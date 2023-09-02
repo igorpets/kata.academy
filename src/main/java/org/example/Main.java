1e61995a94b894cd047991310af1ad6aa028ac98
Igor Pets <b_ear@mail.ru>
2023-09-02 22:52:11 +0300

    next 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args)
     {
@@ -24,5 +26,20 @@ public class Main {
         myChar = 'A';
         System.out.println("Hello world 1 "+myByte+" "+ myShort+" "+myInt+" "+ myLong+" !");
         System.out.println("Hello world 2 "+myFloat+" "+ myDouble+" "+myChar+" "+ myBool+" !");
+
+        Person person1 = new Person();
+        person1.name = "Egor";
+        person1.age = 34;
+        System.out.println("Меня зовут = " + person1.name+", мне "+person1.age +" лет.");
+
+        Person person2 = new Person();
+        person2.name = "Михаил";
+        person2.age = 23;
+        System.out.println("Меня зовут = " + person2.name+", мне "+person2.age +" лет.");
     }
+}
+
+class Person {
+    String name;
+    int age;
 }
