ae8b2e81a6886085713d0693b65a4b94dfef59f3
Igor Pets <b_ear@mail.ru>
2023-09-02 22:38:35 +0300

    next 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,24 @@ public class Main {
     {
         int myInt;
         short myShort;
+        byte myByte;
         long myLong;
         boolean myBool;
-        myInt = 57;
-        myShort = 123;
-        myLong = 12312345392L;
-        myBool = true;
-        System.out.println("Hello world "+myInt+" "+ myShort+" "+myLong+" "+ myBool+" !");
+        double myDouble;
+        float myFloat;
+        char myChar;
+        myInt = 570101030;
+        myShort = 32321;
+        myByte = 34;
+        myLong = 92312345123921212L;
+        if (myByte > 15)
+            myBool = true;
+        else
+            myBool = false;
+        myDouble = 1.002;
+        myFloat = 2.008f;
+        myChar = 'A';
+        System.out.println("Hello world 1 "+myByte+" "+ myShort+" "+myInt+" "+ myLong+" !");
+        System.out.println("Hello world 2 "+myFloat+" "+ myDouble+" "+myChar+" "+ myBool+" !");
     }
 }
