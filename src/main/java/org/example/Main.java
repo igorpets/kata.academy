d54828c9e5daaf7df85bf86a9590ec01a6bbc360
Igor Pets <b_ear@mail.ru>
2023-09-02 22:23:07 +0300

    first

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        int myInt;
+        short myShort;
+        long myLong;
+        myInt = 57;
+        myShort = 123;
+        myLong = 12312345392L;
+        System.out.println("Hello world "+myInt+" "+ myShort+" !");
+    }
+}
